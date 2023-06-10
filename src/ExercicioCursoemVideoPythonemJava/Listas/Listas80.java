33fd6faaa9c96b6672ca785275c3252d98640ace
Matheus Fernandes <matthewoaklucky@gmail.com>
2023-06-10 18:09:23 -0300

    Resolvido ex 80 e 81

--- src/ExercicioCursoemVideoPythonemJava/Listas/Listas80.java
+++ src/ExercicioCursoemVideoPythonemJava/Listas/Listas80.java
@@ -1,5 +1,30 @@
 package ExercicioCursoemVideoPythonemJava.Listas;
+import java.util.ArrayList;
+import java.util.Scanner;
 /* Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista
 , já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela. */
 public class Listas80 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        int valor;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Adicione um número a lista:");
+            valor = scanner.nextInt();
+
+            if (i == 0 || valor > lista.get(lista.size() - 1)) {
+                lista.add(valor);
+            } else {
+                for (int j = 0; j < lista.size(); j++) {
+                    if (lista.get(j) > valor) {
+                        lista.add(j, valor);
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(lista);
+
+    }
 }
--- src/ExercicioCursoemVideoPythonemJava/Listas/Listas81.java
+++ src/ExercicioCursoemVideoPythonemJava/Listas/Listas81.java
@@ -1,8 +1,33 @@
 package ExercicioCursoemVideoPythonemJava.Listas;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 /* Crie um programa que vai ler vários números e colocar em uma lista.
  Depois disso, mostre
  A) Quantos números foram digitados.
 B) A lista de valores, ordenada de forma decrescente.
 C) Se o valor 5 foi digitado e está ou não na lista. */
 public class Listas81 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        int valor = 0;
+
+        while (valor != 999){
+            System.out.println("Adicione um número a lista:");
+            valor = scanner.nextInt();
+            lista.add(valor);
+        }
+        lista.remove(lista.size() - 1);
+        System.out.println("Tamanho da lista = " + lista.size());
+        lista.sort(Collections.reverseOrder());
+        System.out.println(lista);
+        if (lista.contains(5)) {
+            System.out.println("A lista contém o número 5");
+        } else {
+            System.out.println("A lista não contém o número 5");
+        }
+    }
 }
