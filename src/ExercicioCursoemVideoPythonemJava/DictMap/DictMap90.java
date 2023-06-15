571302674dacafb87c704f97d4c204937545e082
Matheus <matheuscif@edu.unifil.br>
2023-06-14 21:44:27 -0300

    ex90 e comeco 91

--- src/ExercicioCursoemVideoPythonemJava/DictMap/DictMap90.java
+++ src/ExercicioCursoemVideoPythonemJava/DictMap/DictMap90.java
@@ -1,7 +1,39 @@
 package ExercicioCursoemVideoPythonemJava.DictMap;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 /*
 Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
 No final, mostre o conteúdo da estrutura na tela.
 * */
 public class DictMap90 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> boletim = new HashMap<>();
+
+        while (true) {
+            System.out.println("Digite o nome do aluno:");
+            String nome = scanner.next();
+
+            if (nome.equalsIgnoreCase("sair")) {
+                break;
+            } 
+            
+            System.out.println("Digite a nota de " + nome);
+            double nota = scanner.nextDouble();
+
+            if (nota < 7.0) {
+                boletim.put(nome, "Reprovado");
+            } else {
+                boletim.put(nome, "Aprovado");
+            }
+
+
+        }
+
+        System.out.println(boletim.entrySet());
+
+        scanner.close();
+    }
 }
--- src/ExercicioCursoemVideoPythonemJava/DictMap/DictMap91.java
+++ src/ExercicioCursoemVideoPythonemJava/DictMap/DictMap91.java
@@ -1,6 +1,36 @@
 package ExercicioCursoemVideoPythonemJava.DictMap;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.security.SecureRandom;
 /*  Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
-dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+dicionário em java. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 */
 public class DictMap91 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        SecureRandom random = new SecureRandom();
+
+        Map<String, Integer> jogo = new HashMap<>();
+
+        String player1 = "player1";
+        String player2 = "player2";
+        String player3 = "player3";
+        String player4 = "player4";
+
+        int dado1 = random.nextInt(6 + 1);
+        int dado2 = random.nextInt(6 + 1);
+        int dado3 = random.nextInt(6 + 1);
+        int dado4 = random.nextInt(6 + 1);
+
+        
+
+        jogo.put(player1, dado1);
+        jogo.put(player2, dado2);
+        jogo.put(player3, dado3);
+        jogo.put(player4, dado4);
+
+
+
+    }
 }
