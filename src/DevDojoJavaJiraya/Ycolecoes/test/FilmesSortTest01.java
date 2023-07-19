0583bd65372857e7138dad09c44bc2001d686ea4
mattcif <matthewoaklucky@gmail.com>
2023-07-19 10:15:42 -0300

    Sort id usando comparator

--- src/DevDojoJavaJiraya/Ycolecoes/test/FilmesSortTest01.java
+++ src/DevDojoJavaJiraya/Ycolecoes/test/FilmesSortTest01.java
@@ -4,8 +4,15 @@ import DevDojoJavaJiraya.Ycolecoes.dominio.Filme;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+class ManageByIdComparator implements Comparator<Filme> {
+    @Override
+    public int compare(Filme filme1, Filme filme2) {
+        return filme1.getId().compareTo(filme2.getId());
+    }
+}
 public class FilmesSortTest01 {
     public static void main(String[] args) {
         List<Filme> filmes = new ArrayList<>(6);
@@ -22,6 +29,13 @@ public class FilmesSortTest01 {
         // Comparable Sort
         Collections.sort(filmes);
         System.out.println("---------------------");
+        for (Filme filme : filmes) {
+            System.out.println(filme);
+        }
+
+        // Comparator Sort
+        filmes.sort(new ManageByIdComparator());
+        System.out.println("-------------------");
         for (Filme filme : filmes) {
             System.out.println(filme);
         }
