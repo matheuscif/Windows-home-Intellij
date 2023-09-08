af1c8d7f145e989f2ce230eb272060909ea8ade4
mattcif <matthewoaklucky@gmail.com>
2023-09-08 16:29:52 -0300

    Thread wait e notify, entraga de emails

---
+++ src/DevDojoJavaJiraya/ZZFthreads/test/EmailDeliveryTest01.java
@@ -0,0 +1,25 @@
+package DevDojoJavaJiraya.ZZFthreads.test;
+
+import DevDojoJavaJiraya.ZZFthreads.dominio.Members;
+import DevDojoJavaJiraya.ZZFthreads.service.EmailDeliveryService;
+
+import javax.swing.*;
+
+public class EmailDeliveryTest01 {
+    public static void main(String[] args) {
+        Members members = new Members();
+        Thread jiraya = new Thread(new EmailDeliveryService(members), "Jiraya");
+        Thread kakashi = new Thread(new EmailDeliveryService(members), "Kakashi");
+        jiraya.start();
+        kakashi.start();
+
+        while(true) {
+            String email = JOptionPane.showInputDialog("Entre com seu email");
+            if (email == null || email.isEmpty()) {
+                members.close();
+                break;
+            }
+            members.addMemberEmail(email);
+        }
+    }
+}
